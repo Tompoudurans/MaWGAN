c9467ae921a0f97719d52398ed646e8b98ba6854
Thomas Poudevigne-Durance <tompoudevigne@gmail.com>
2020-01-28 14:53:46 +0000

    now plot the results like tcgan

--- ganrunner.py
+++ ganrunner.py
@@ -1,12 +1,15 @@
 from sklearn import datasets
 from tengan import dataGAN
+from dataman import plotting
 import numpy as np
 batch = 100
-iris = datasets.load_wine()
+iris = datasets.load_iris()
 no_field = len(iris.data[1])
 mygan = dataGAN('adam',batch,no_field,batch)
 mygan.discriminator.summary()
 mygan.model.summary()
 mygan.train(iris.data,batch,1000)
 noise = np.random.normal(0, 1, (100, 100))
-print(mygan.generator.predict(noise))
+generated_data = mygan.generator.predict(noise)
+print(generated_data)
+plotting(generated_data)
