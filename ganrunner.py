2e1f97ab427cf3cc29b4f5c790e31cfcc9bd606e
Thomas Poudevigne-Durance <tompoudevigne@gmail.com>
2020-02-07 11:19:47 +0000

    cheage the opti to RMSprop

--- ganrunner.py
+++ ganrunner.py
@@ -1,16 +1,16 @@
 from sklearn import datasets
 from tengan import dataGAN
-from dataman import plotting,show_loss_progress
+from dataman import dagpolt,show_loss_progress
 import numpy as np
 batch = 100
 iris = datasets.load_iris()
 no_field = len(iris.data[1])
-mygan = dataGAN('adam',batch,no_field,batch)
+mygan = dataGAN('RMSprop',batch,no_field,batch)
 mygan.discriminator.summary()
 mygan.model.summary()
 mygan.train(iris.data,batch,1000)
 noise = np.random.normal(0, 1, (100, 100))
 generated_data = mygan.generator.predict(noise)
 print(generated_data)
-plotting(generated_data,iris.data)
+dagpolt(generated_data,iris.data)
 show_loss_progress(mygan.d_losses,mygan.g_losses)
