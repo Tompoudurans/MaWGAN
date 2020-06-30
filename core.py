07080689bf7953a24d2f600fc0b319b24c54fb7c
Thomas Poudevigne <drtrom95@gmail.com>
2020-06-30 15:15:21 +0100

    the code is more flexable
    the number_of_core can be chosen for each run

--- core.py
+++ core.py
@@ -1,10 +1,14 @@
 import os
 import tensorflow as tf
 
-def set_core():
-    os.environ['MKL_NUM_THREADS'] = '2'
-    os.environ['GOTO_NUM_THREADS'] = '2'
-    os.environ['OMP_NUM_THREADS'] = '2'
-    #os.environ['openmp'] = 'False'
-    tf.config.threading.set_inter_op_parallelism_threads(2) #Set number of threads used for parallelism between independent operations.
-    tf.config.threading.set_intra_op_parallelism_threads(2) #Set number of threads used within an individual op for parallelism.
+def set_core(number_of_core):
+    """
+    deduce the maximum number of cores that code can uses
+    """
+    os.environ['MKL_NUM_THREADS'] = str(number_of_core)
+    os.environ['GOTO_NUM_THREADS'] = str(number_of_core)
+    os.environ['OMP_NUM_THREADS'] = str(number_of_core)
+    #Set number of threads used for parallelism between independent operations.
+    tf.config.threading.set_inter_op_parallelism_threads(number_of_core)
+    #Set number of threads used within an individual op for parallelism.
+    tf.config.threading.set_intra_op_parallelism_threads(number_of_core)
