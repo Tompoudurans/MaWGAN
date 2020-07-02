b22df7a40117bdea91e698e631385d79447bdd12
Thomas Poudevigne <drtrom95@gmail.com>
2020-07-02 15:27:53 +0100

    remove dupicated test files

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -6,22 +6,34 @@ Created on Mon Jun  1 15:31:20 2020
 @author: c1751832
 
 """
-
-from data.prepocessing import unnormalize
-from fid import calculate_fid
+import numpy
+from src.tools.prepocessing import unnormalize
+from src.tools.fid import calculate_fid
+from src.main.tengan import dataGAN
+layers = 5
+nodes = 20
+data = 3
+batch_size = 2
+noise_vector = 10
+testgan = dataGAN('adam', noise_vector, data, nodes, layers)
 
 def test_fid():
     """
     testing fid distance it value should be greater than 0
     """
+    testgan.load_weights('tests/testing')
+    dataset=numpy.array([[1.0,1.2,1.3],[2.1,2.2,2.3]])
     data = testgan.create_fake(batch_size)
     f = calculate_fid(data,dataset)
     assert f > 0
 
-def broken_test_norm():
+def test_norm():
     """
-    #testing  unnormalize funtion it should give the orginal data
-    #"""
+    testing  unnormalize funtion it should give the orginal data
+    """
+    dataset=numpy.array([2.1,2.2,2.3])
     n = unnormalize(dataset,6,2)
-    exp = numpy.array([[8.2, 8.4, 8.6],[10.2,10.4,10.6]])
-    assert all(n == exp)
+    n = numpy.array(n)
+    exp = numpy.array([10.2,10.4,10.6])
+    for i in range(len(n)):
+        assert n[i] == exp[i]
--- tests/unfiniched_test_main.py
+++ tests/unfiniched_test_main.py
@@ -1,17 +1,18 @@
 import main
-
-
-def test_setup():
-    pass
+import numpy
 
 def test_load_data():
-    pass
+    loaded_data = load_data(sets,'test.db')
+    dataset=numpy.array([[1.0,1.2,1.3],[2.1,2.2,2.3]])
+    assert loaded_data == dataset
+
 
 def test_show_samples():
     pass
 
 def test_parameters_handeling():
-    pass
-
-def test_marathon_mode():
-    pass
+    #saves
+    saved_parameters, successfully_loaded = parameters_handeling('tests/testing', parameters_list)
+    #loads
+    loaded_parameters, successfully_loaded = parameters_handeling('tests/testing', None)
+    assert loaded_parameters == saved_parameters
