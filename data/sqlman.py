fc84156fbaed98bfd3e08fdbbcd5c62b8fae458b
Thomas Poudevigne <drtrom95@gmail.com>
2020-07-02 17:49:24 +0100

    write a funtion that handel categorical data

--- data/sqlman.py
+++ data/sqlman.py
@@ -3,17 +3,32 @@ from src.tools.prepocessing import get_norm
 import pandas as pd
 
 def load_sql(file,table):
+    """
+    Loads an SQL table and pre-procsses the table, ready to be trained
+    """
     engine = sa.create_engine('sqlite:///' + file) #creates an empty database
     connection = engine.connect()
     database = pd.read_sql(table, connection)
-    # write a line to handel categorical
+    database,idexes = factorizing(database)
     database = database.dropna()
     database,mean,std = get_norm(database)
     return database,mean,std
 
 
 def save_sql(df):
+    """
+    Saves the generated data to a SQL table - although now it doesnt seem to work
+    """
     engine = sa.create_engine('sqlite:///',echo=False)
     df=df.drop(columns=['dataset'])
     df.to_sql('users', con=engine, if_exists='append')
     engine.execute("SELECT * FROM users").fetchall()
+
+def factorizing(data):
+    indexs = []
+    for name in data.columns:
+        if 'O' == data[name].dtype:
+            new = pd.factorize(data[name])
+            data[name] = new[0]
+            indexs.append(new[1])
+    return data,indexs
