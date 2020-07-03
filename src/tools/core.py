007df9de3f4d3f82f27846e230a4fcf587b65af9
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-03 14:02:32 +0100

    clear the doc
    better grammar and better defintion on doc string
    remove unnessary comments

--- src/tools/core.py
+++ src/tools/core.py
@@ -1,15 +1,13 @@
 import os
 import tensorflow as tf
 
-def set_core(number_of_core):
+
+def set_core(number_of_cores):
     """
-    deduce the maximum number of cores that code uses
+    Set the number of cores.
     """
-    os.environ['MKL_NUM_THREADS'] = str(number_of_core)
-    os.environ['GOTO_NUM_THREADS'] = str(number_of_core)
-    os.environ['OMP_NUM_THREADS'] = str(number_of_core)
-    #os.environ['openmp'] = 'False'
-    #Set number of threads used for parallelism between independent operations.
-    tf.config.threading.set_inter_op_parallelism_threads(number_of_core)
-    #Set number of threads used within an individual op for parallelism.
-    tf.config.threading.set_intra_op_parallelism_threads(number_of_core)
+    os.environ["MKL_NUM_THREADS"] = str(number_of_cores)
+    os.environ["GOTO_NUM_THREADS"] = str(number_of_cores)
+    os.environ["OMP_NUM_THREADS"] = str(number_of_cores)
+    tf.config.threading.set_inter_op_parallelism_threads(number_of_cores)
+    tf.config.threading.set_intra_op_parallelism_threads(number_of_cores)
