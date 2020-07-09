834e5b7b708d1233ef72fc129f3ff6bcd0721b8a
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-09 15:34:54 +0100

    add docstring

--- src/tools/prepocessing.py
+++ src/tools/prepocessing.py
@@ -1,6 +1,10 @@
 import pandas
 
 def import_penguin(file,use_categorical):
+    """
+    Imports the penguin dataset then processes the data so that it is ready to be trained.
+    It sets the categorical data into numerical data
+    """
     penguin = pandas.read_csv(file)
     penguin = penguin.dropna()
     if use_categorical:
@@ -9,22 +13,31 @@ def import_penguin(file,use_categorical):
         penguin = penguin.replace({'Dream':0,'Torgersen':1,'Biscoe':2})
     else:
         penguin = penguin.drop(columns=['sex','species','island'])
-    col = penguin.columns    
+    col = penguin.columns
     penguin,mean,std = get_norm(penguin)
     return penguin,mean,std,col
 
 def normalize(dataset,mean,std):
+    """
+    Normalises the dataset by mean and standard deviation
+    """
     mid = dataset - mean
     new_data = mid / std
     return new_data
 
 def unnormalize(dataset,mean,std):
+    """
+    Reverts the normalised dataset to original format
+    """
     df = pandas.DataFrame(dataset)
     mid = df*std
     original = mid + mean
     return original
 
 def get_norm(data):
+    """
+    Provides the mean and standard deviation for the dataset so it can be normalised.
+    """
     mean = data.mean()
     std = data.std()
     data = normalize(data,mean,std)
--- src/tools/sqlman.py
+++ src/tools/sqlman.py
@@ -2,20 +2,26 @@ import sqlalchemy as sa
 from src.tools.prepocessing import get_norm
 import pandas as pd
 
-def load_sql(file,table):
-    engine = sa.create_engine('sqlite:///' + file) #creates an empty database
+def load_sql(file, table):
+    """
+    Loads an SQL table and pre-procsses the table, ready to be trained
+    """
+    engine = sa.create_engine("sqlite:///" + file + ".db")
     connection = engine.connect()
     database = pd.read_sql(table, connection)
-    # write a line to handel categorical
+    database, idexes = factorizing(database)
     database = database.dropna()
-    col = database.columns
-    database,mean,std,col = get_norm(database)
-    return database
+    database, mean, std = get_norm(database)
+    return database, mean, std ,idexes
 
 
-def save_sql(df):
-    engine = sa.create_engine('sqlite:///',echo=False)
-    df=df.drop(columns=['dataset'])
-    df.to_sql('users', con=engine, if_exists='append')
-    engine.execute("SELECT * FROM users").fetchall()
- 
+def save_sql(df,file):
+    """
+    Saves the generated data to a SQL table called generated_data
+    """
+    engine = sa.create_engine("sqlite:///" + file + ".db")
+    try:
+        df = df.drop(columns=["dataset"])
+    except KeyError:
+        pass
+    df.to_sql("generated_data", con=engine, if_exists="append")
