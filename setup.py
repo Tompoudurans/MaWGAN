ecaa6913ef45db4c5728932e300bb9ee3889a795
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-10 15:42:03 +0100

    setup.py is now working

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
 from setuptools import setup, find_packages
 
-gans_requirements = [tensorflow,numpy]
-
-tools_requirments = 
-
 setup(
-    name="gans",
+    name="ganrunner",
     version="0.0.1",
-    install_requires=requirements,
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-)
-
-setup(
-    name="tools",
-    version="0.0.1",
-    install_requires=requirements,
     packages=find_packages("src"),
     package_dir={"": "src"},
 )
