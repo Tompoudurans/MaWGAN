6d8aeaf981d2313a40d35ef76504a3224499531e
thomas poudevigne-durance <drtrom95@gmail.com>
2020-08-28 12:20:45 +0100

    created test fo the user interface
    notes: the test will pause for grathis fixed in imporve grathics
    commeted out lines are for when setup file is up

---
+++ tests/randomdatagen.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+from random import uniform, randint
+import sqlalchemy as sa
+import pandas as pd
+
+def generate_random_testing_data(size):
+    """
+    create random testing data 3 numrical
+    """
+    engine = sa.create_engine('sqlite:///flight.db')
+    connection = engine.connect()
+    Reading = namedtuple('Reading', 'temp, pressure, humidity')
+
+    sql = """
+    CREATE TABLE readings (
+        temp      NUMERIC(3,1) NOT NULL,
+        pressure  NUMERIC(4,0) NOT NULL,
+        humidity  NUMERIC(3,0) NOT NULL,
+
+        CONSTRAINT temp_ck CHECK (temp BETWEEN -70 AND 70),
+        CONSTRAINT pres_ck CHECK (pressure BETWEEN 0 AND 2000),
+        CONSTRAINT hum_ck CHECK (humidity BETWEEN 0 AND 100)
+    )
+    """
+    connection.execute(sql)
+    readings = [
+        Reading(
+            temp=round(uniform(23, 27), 1),
+            pressure=randint(1020, 1050),
+            humidity=randint(30, 50))
+        for i in range(size)]
+
+    sql = """
+        INSERT INTO readings
+            (temp, pressure, humidity)
+        VALUES
+            (?, ?, ?)
+    """
+
+    for reading in readings:
+        values = (reading.temp, reading.pressure, reading.humidity)
+        connection.execute(sql, values)
+    return pd.read_sql('readings',connection)
---
+++ tests/test_main.py
@@ -0,0 +1,54 @@
+import subprocess
+import os
+#import main as ganrunner
+from randomdatagen import generate_random_testing_data
+#import ganrunner
+
+def test_help():
+    exp_output=b"""Usage: main.py [OPTIONS]\n
+  This code creates and trains a GAN. Core elements of this code are sourced
+  directly from the David Foster book 'Deep generative models' and have been
+  modified to work with a numeric database such as the iris datasets taken
+  from the library 'sklearn'. Soon this GAN should work on the DCWW dataset.\n\nOptions:
+  --mode TEXT       mode?(s)pyder/(n)ormal/(m)arathon)
+  --filepath TEXT   enter the file name and location of the database and model
+  --epochs TEXT     choose how long that you want to train
+  --dataset TEXT    choose the dataset/table that the GAN will train on - this
+                    can't be a single letter - don't add .db\n
+  --model TEXT      choose which model you what to use
+  --opti TEXT       choose the optimiser you want to use
+  --noise TEXT      choose the length of the noise vector
+  --batch TEXT      choose how many fake data you want to make in one go
+  --layers TEXT     choose the number of layers of each network
+  --clip TEXT       if using WGAN choose the clipping threshold
+  --core INTEGER    select the number of cores that you would like to run
+  --sample INTEGER  choose the number of generated data you want:
+                    (samples*batch)\n
+  --help            Show this message and exit.\n"""
+    #currunt_output=subprocess.run(["python","-m","ganrunner","--help"],capture_output=True)
+    currunt_output=subprocess.run(["python","main.py","--help"],capture_output=True)
+    assert currunt_output.stdout == exp_output
+
+def test_normal_run():
+    generate_random_testing_data(50)
+    file_size = os.stat('flight.db').st_size
+    #status=subprocess.run(["python","-m","ganrunner",
+    status=subprocess.run(["python","main.py",
+    "--model=g",
+    "--filepath=flight",
+    "--opti=adam",
+    "--noise=50",
+    "--batch=50",
+    "--layers=3",
+    "--epochs=10",
+    "--dataset=readings"
+    ])
+    assert status.returncode == 0
+    assert os.path.isfile('flight_model.h5')
+    assert os.path.isfile('flight_generator.h5')
+    assert os.path.isfile('flight_discriminator.h5')
+    assert os.path.isfile('flight_parameters.npy')
+    assert file_size < os.stat('flight.db').st_size and (file_size > 0)
+
+def test_marathon():
+    pass
