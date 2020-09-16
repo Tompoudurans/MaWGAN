859e64a58ad65b38d756be7cb09258db7d242e15
thomas poudevigne-durance <drtrom95@gmail.com>
2020-09-16 11:51:41 +0100

    started gp investigation

---
+++ gp/keras.py
@@ -0,0 +1,41 @@
+from sklearn import datasets
+from tendupden import WGANGP
+from dataman import show_loss_progress
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as mp
+
+def dagplot(x,y):
+    fake = pd.DataFrame(x)
+    real = pd.DataFrame(y)
+    fake['dataset'] = ['fake']*len(x)
+    real['dataset'] = ['real']*len(y)
+    result = pd.concat([real, fake])
+    sns.pairplot(result,hue='dataset')
+    mp.show()
+
+
+batch = 150
+iris = datasets.loa#from tengan import dataGANd_iris()
+no_field = len(iris.data[1])
+#mygan = dataGAN('adam',batch,no_field,batch)
+mygan = WGANGP(input_dim = no_field
+        , critic_dropout_rate = '?'
+        , critic_learning_rate =0.4
+        , generator_initial_dense_layer_size = 150
+        , generator_dropout_rate = '?'
+        , generator_learning_rate = 0.4
+        , optimiser = 'adam'
+        , grad_weight = 1
+        , z_dim = 0
+        , batch_size = batch
+        )
+mygan.critic.summary()
+mygan.model.summary()
+mygan.train(iris.data,batch,500,'wgangp/',10,5)
+noise = np.random.normal(0, 1, (150, 150))
+generated_data = mygan.generator.predict(noise)
+print(generated_data)
+dagplot(generated_data,iris.data)
+show_loss_progress(mygan.d_losses,mygan.g_losses)
