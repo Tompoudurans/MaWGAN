4f65f21e29ddeebb195e16ec7933f1cd2cb5cad4
Thomas Poudevigne <drtrom95@gmail.com>
2020-10-12 11:16:09 +0100

    added decoding

--- src/ganrunner/tools/categorical.py
+++ src/ganrunner/tools/categorical.py
@@ -1,10 +1,11 @@
 import pandas
+import numpy
 
 def encoding(data):
     """
     Transforms categorical data into numerical, saves maping on a list.
     """
-    details = []
+    details = [len(data.columns)]
     for name in data.columns:
         if "O" == data[name].dtype:
             new = pandas.get_dummies(data[name])
@@ -14,4 +15,22 @@ def encoding(data):
     return data, details
 
 def decoding(data,details):
-    pass
+    print('point')
+    col_len = len(data.columns)
+    position = details[0] - len(details) + 1
+    start = position
+    current = 1
+    while position < col_len:
+        end = position + details[current][1]
+        set_of_cat = data.iloc[:,position:end]
+        restore = []
+        for value in range(set_of_cat.shape[0]):
+            try:
+                restore.append(set_of_cat.ix[value].idxmax())
+            except TypeError:
+                restore.append(None)
+        data[details[current][0]] = restore
+        current = current + 1
+        position = end
+    data = data.drop(:,start:end)
+    return data
--- src/ganrunner/tools/test_categorical.py
+++ src/ganrunner/tools/test_categorical.py
@@ -4,3 +4,6 @@ import pandas
 data = pandas.read_csv('penguins_size.csv')
 new,bit = categorical.encoding(data)
 print('new\n',new,'\nlist ',bit)
+
+stuff = categorical.decoding(new,bit)
+print(stuff)
