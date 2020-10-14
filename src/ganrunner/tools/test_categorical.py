c942215083ae1c7bbd7149ff2df06598c11ead62
Thomas Poudevigne <drtrom95@gmail.com>
2020-10-14 12:23:20 +0100

    improve tesing and move to correct folder

--- src/ganrunner/tools/test_categorical.py
+++
@@ -1,9 +0,0 @@
-import categorical
-import pandas
-
-data = pandas.read_csv('penguins_size.csv')
-print(data)
-new,bit = categorical.encoding(data)
-
-stuff = categorical.decoding(new,bit)
-print(stuff)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -52,18 +52,12 @@ def test_dagplot():
     assert os.path.isfile("test_compare.pdf")
 
 
-def test_factorizing():
-    """
-    Tests the handling of the categorical data, this function should transform
-    all categorical data in to numbers and provide an index
-    """
-    data = pandas.DataFrame({"num": [1.0, 1.2, 1.3], "cat": ["A", "B", "A"]})
-    newdata, indexs = ganrunner.factorizing(data)
-    expected_index = numpy.array(["A", "B"], dtype="object")
-    expected_data = pandas.DataFrame({"num": [1.0, 1.2, 1.3], "cat": [1, 2, 1]})
-    assert all(newdata == expected_data)
-    assert all(indexs[0] == expected_index)
-
+def test_encoding():
+    data = pandas.read_csv('penguins_size.csv')
+    new,bit = ganrunner.tools.encoding(data)
+    assert new.dtypes.all() == 'uint8'
+    stuff = ganrunner.tools.decoding(new,bit)
+    assert len(stuff) == len(data)
 
 def test_sql_load_and_save():
     """
