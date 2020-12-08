1cad42a95a684ed92af262d2b0ba7159d457c9aa
Thomas poudevigne <drtrome95@gmail.com>
2020-12-08 11:06:37 +0000

    remove test files

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = []
+    for library in f.read().splitlines():
+        requirements.append(library)
 setup(
     name="ganrunner",
     version="0.0.4",
