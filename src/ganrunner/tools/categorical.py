87fd8ad37ec251f10be44a789bcf1a77856cebda
thomas poudevigne <drtrom95@gmail.com>
2021-03-09 15:02:31 +0000

    remove the specific binary fuction
    as was not woking on large db also added count of catigorical

--- src/ganrunner/tools/categorical.py
+++ src/ganrunner/tools/categorical.py
@@ -6,17 +6,15 @@ def encoding(data):
     Transforms categorical data into numerical, saves maping on a list.
     """
     details = [len(data.columns)]
+    count_o = 0
     for name in data.columns:
         if "O" == data[name].dtype:
             new = pandas.get_dummies(data[name])
             data[new.columns] = new
             data = data.drop(columns=name)
             details.append([name, len(new.columns)])
-        if data[name].max() == 1 and data[name].min() == 0:
-            new = data[name]
-            data = data.drop(columns=name)
-            data[name] = new
-            details.append([name, 1])
+            count_o =+ 1
+    print("there are",count_o,"categorical data variables")
     return data, details
 
 
