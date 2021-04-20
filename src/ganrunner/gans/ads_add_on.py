9b84ddcbf2fa9d924b6aa6a3d98349ddc422e941
thomas poudevigne <drtrom95@gmail.com>
2021-04-20 11:31:55 +0100

    finish m-distance

--- src/ganrunner/gans/ads_add_on.py
+++ src/ganrunner/gans/ads_add_on.py
@@ -1,4 +1,19 @@
 import torch
+import numpy
+
+def vardag(x):
+    li= []
+    for i in x:
+         torch.var(x)
+         li.append(torch.var(i))
+    matrix = torch.tensor(li)
+    return matrix.diag()
+    
+def vardrag(x):
+    covmat = numpy.cov(x)
+    varvec = covmat.diagonal()
+    vartensor = torch.tensor(varvec)
+    return vartensor.diag()
 
 def weighted_entopy(x,y):
     pass
@@ -14,3 +29,5 @@ def mahalanobis_distance(x,y):
     left=right.T
     mid_right = torch.mm(mid,right)
     return torch.mm(left,mid_right)
+
+
