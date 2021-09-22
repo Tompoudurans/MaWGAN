fef9d434a727cc230f49623714185a520bbad6d3
thomas poudevigne <drtrom95@gmail.com>
2021-09-22 13:52:19 +0100

    repalce linear cells by GRU cells

--- src/ganrunner/gans/wgan_gradient_penalty.py
+++ src/ganrunner/gans/wgan_gradient_penalty.py
@@ -48,18 +48,18 @@ class wGANgp(object):
         """
         self.Generator = nn.Sequential()
         self.Generator.add_module(
-            str(number_of_layers) + "layer", nn.Linear(self.net_dim, self.net_dim)
+            str(number_of_layers) + "layer", nn.GRUCell(self.net_dim, self.net_dim)
         )
         self.Generator.add_module(str(number_of_layers) + "active", nn.Tanh())
         number_of_layers -= 1
         while number_of_layers > 1:
             self.Generator.add_module(
-                str(number_of_layers) + "layer", nn.Linear(self.net_dim, self.net_dim)
+                str(number_of_layers) + "layer", nn.GRUCell(self.net_dim, self.net_dim)
             )
             self.Generator.add_module(str(number_of_layers) + "active", nn.Tanh())
             number_of_layers -= 1
         self.Generator.add_module(
-            str(number_of_layers) + "layer", nn.Linear(self.net_dim, self.z_dim)
+            str(number_of_layers) + "layer", nn.GRUCell(self.net_dim, self.z_dim)
         )
         # ------------------------------------------------------------------------------------------------------------------
         # nn.ConvTranspose2d(in_channels=100, out_channels=1024, kernel_size=4, stride=1, padding=0),
@@ -73,18 +73,18 @@ class wGANgp(object):
         """
         self.Critic = nn.Sequential()
         self.Critic.add_module(
-            str(number_of_layers) + "layer", nn.Linear(self.z_dim, self.net_dim)
+            str(number_of_layers) + "layer", nn.GRUCell(self.z_dim, self.net_dim)
         )
         self.Critic.add_module(str(number_of_layers) + "active", nn.Tanh())
         number_of_layers -= 1
         while number_of_layers > 1:
             self.Critic.add_module(
-                str(number_of_layers) + "layer", nn.Linear(self.net_dim, self.net_dim)
+                str(number_of_layers) + "layer", nn.GRUCell(self.net_dim, self.net_dim)
             )
             self.Critic.add_module(str(number_of_layers) + "active", nn.Tanh())
             number_of_layers -= 1
         self.Critic.add_module(
-            str(number_of_layers) + "layer", nn.Linear(self.net_dim, 1)
+            str(number_of_layers) + "layer", nn.GRUCell(self.net_dim, 1)
         )
 
     def create_fake(self, batch_size):
@@ -94,6 +94,13 @@ class wGANgp(object):
         z = torch.randn(batch_size, self.data_dim)
         fake_images = self.Generator(z)
         return fake_images.detach().numpy()
+
+    def linear_sample(self,data):
+        "select samples that are linearly dependent"
+        sizes = len(data) - self.batch_size
+        start_loc = torch.randint(0,sizes,(1,))
+        index = range(start_loc,start_loc + self.batch_size)
+        return data[index]
 
     def train(
         self,
@@ -110,7 +117,6 @@ class wGANgp(object):
         the dataset x_train which has a length of batch_size.
         It will print and record the loss of the generator and critic every_n_batches.
         """
-
         if hasmissing:
             print("missing data mode on")
         self.batch_size = batch_size
@@ -127,9 +133,10 @@ class wGANgp(object):
             # Train Dicriminator forward-loss-backward-update n_critic times while 1 Generator forward-loss-backward-update
             for d_iter in range(n_critic):
                 self.Critic.zero_grad()
-                sample = self.pick_sample(data_tensor)
+                #---------------------------------------------------sample is usless at least not random
+                sample = self.linear_sample(data_tensor)
                 images = Variable(sample)
-                # Train discriminator
+                #Train discriminator
                 z = Variable(torch.randn(self.batch_size, self.data_dim))
                 fake_images = self.Generator(z)
 
