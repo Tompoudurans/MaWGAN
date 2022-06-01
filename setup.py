1cdc36e42a808d36b32e36be08e47e62c5fb367e
Thomas <drtrom95@gmail.com>
2022-06-01 10:48:27 +0100

    seprated instalation

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = []
-    for library in f.read().splitlines():
-        requirements.append(library)
 setup(
     name="ganrunner",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=requirements,
 )
