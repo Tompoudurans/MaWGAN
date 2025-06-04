b4819348d663e9bf9ee81ec6c0540efebbf5b88b
Tompoudurans <59568436+Tompoudurans@users.noreply.github.com>
2025-06-04 09:51:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ganrunner",
-    version="0.0.5",
+    version="0.0.7",
     packages=find_packages("src"),
     package_dir={"": "src"},
 )
